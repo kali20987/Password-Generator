c803058acefab3d922431322c0027641fb8b8fcb
kali19 <khadijah.syed.ali@gmail.com>
2023-04-06 23:48:42 -0400

    comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-// Assignment code here
 
+//main function which determines how long the password must be and if the user clicks ok on at least one of the prompts for their password.
 function main() {
   var password = "";
   var len = getLength();
@@ -18,43 +18,43 @@ function main() {
   writePassword(password);
 }
 
-// Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword(password) {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
+//getLength function which prompts the first box asking how long the user wants their password.
 function getLength() {
   return prompt("How long would you like your password?");
 }
-
+//showLengthError function that sends an alert box if the user's password doesn't meet the requirements.
 function showLengthError() {
   alert("Bad password length!");
 }
-
+//getLowerCase function which asks the user if they want lowercase letters in their password.
 function getLowerCase() {
   return confirm("Click OK if you would like lowercase letters.");
 }
-
+//getUpperCase function which asks the user if they want uppercase letters in their password.
 function getUpperCase() {
   return confirm("Click OK if you would like uppercase letters");
 }
-
+//getNum function which asks the user if they want numbers in their password.
 function getNum() {
   return confirm("Click OK if you would like numbers");
 }
-
+//getSpec function which asks the user if they want special characters in their password.
 function getSpec() {
   return confirm("Click OK if you would like special characters");
 }
-
+//showCharacterError function which pops up if the user clicked cancel for all prompts.
 function showCharacterError() {
   alert("At least one character type must be selected");
 }
-
+//generatePassword function which uses the pervious variables and new variables to generate a password based on what the user selected.
 function generatePassword(len, lcase, ucase, num, spec) {
   var lower = 'abcdefghijklmnopqrstuvwxyz';
   var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -85,5 +85,4 @@ function generatePassword(len, lcase, ucase, num, spec) {
 
   return result;
 }
-// Add event listener to generate button
 generateBtn.addEventListener("click", main);
